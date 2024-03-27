009c785e3ec3cd194dc3994e7c74023b45489fc1
Sugapratha <102165547+Sugapratha@users.noreply.github.com>
2024-03-27 18:16:02 +0530

    list.py

---
+++ list.py
@@ -0,0 +1,18 @@
+
+# LISTING
+
+# Insert
+my_list=['apple','banana','cherry','date']
+my_list.insert(2,'grape')
+print(my_list)
+
+# Append
+my_list.append('elderberry')
+print(my_list)
+
+# Mylist
+print("My list is:",my_list)
+
+# Sliced List
+list=my_list[2:5]
+print("Sliced list is:",list)
